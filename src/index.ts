a32ef1dbe7a90e7b588afd7cb0bd9429f91635d9
minhyeok hwang <koey2182@gmail.com>
2025-06-03 20:33:55 +0900

    feat: 불변 객체 예시 작성

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello, World!!!");
+import { createImutableList } from './examples/imutable-list';
+
+const imutableList = createImutableList(['a', 'b', 'c']);
+console.log(imutableList.getData()); // [ 'a', 'b', 'c' ]
+
+const newImutableList = imutableList.push('123').push('456').push('789');
+console.log(imutableList.getData()); // [ 'a', 'b', 'c' ]
+console.log(newImutableList.getData()); // [ 'a', 'b', 'c', '123', '456', '789' ]
