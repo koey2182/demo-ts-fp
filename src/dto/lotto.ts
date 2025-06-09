2438164339f87386547768cf048fec2f286518b8
unknown <koey2182@gmail.com>
2025-06-10 07:46:46 +0900

    feat: 로또 추첨 기능 구현

---
+++ src/dto/lotto.ts
@@ -0,0 +1,47 @@
+import { objectType } from "nexus";
+import { randomInt } from "node:crypto";
+
+export type Lotto = {
+    numbers: number[];
+    additional: number;
+}
+
+export const LottoTypeDef = objectType({
+    name: 'Lotto',
+    description:
+        '1~45의 숫자 중 랜덤한 6개의 숫자와 1개의 추가 번호를 포함하는 로또입니다. 총 7개의 숫자는 모두 중복이 없습니다.',
+    definition(t) {
+        t.nonNull.list.nonNull.int('numbers', {
+            description:
+                '1~45 중 랜덤 6개의 숫자를 추출한 배열입니다. 각 숫자는 중복되지 않습니다.',
+        });
+        t.nonNull.int('additional', {
+            description:
+                '1~45 중 하나를 추출한 추가 번호입니다. 앞선 6개의 숫자와 중복되지 않습니다.',
+        });
+    },
+});
+
+const shuffle: <T>(list: T[], count: number) => T[] = (list, count) => {
+    const result = [...list];
+
+    for (let i = 0; i < result.length; i++) {
+        const j = randomInt(i, result.length);
+        [result[i], result[j]] = [result[j], result[i]];
+    }
+
+    return result.slice(0, count);
+}
+
+export const makeLotto: () => Lotto = () => {
+    const nums = Array.from({length: 45}, (_, i) => i + 1);
+    const shuffled = shuffle(nums, 7);
+    const additional = shuffled.pop()!;
+    const numbers = shuffled.sort((a, b) => a - b);
+
+    return {
+        numbers,
+        additional
+    }
+}
+
