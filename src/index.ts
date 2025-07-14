c14de08a6050754f121d1780a277330fde95fd11
unknown <koey2182@gmail.com>
2025-07-14 09:16:24 +0900

    refactor: 포트 번호 기본값 80

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { createApp, run } from './api/app';
 import { config } from 'dotenv';
 
-const port = parseInt(process.env.PORT ?? '3000');
+const port = parseInt(process.env.PORT ?? '80');
 
 createApp().then((app) => {
     run(app, { port });
